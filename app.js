7b8848946fb816ffceddfbffac4d71c1c032ca59
lsmucassi <lindasmucassi@gmail.com>
2020-04-28 17:36:20 +0200

    solve for hit and random move up

---
+++ app.js
@@ -0,0 +1,26 @@
+const square = document.querySelectorAll('.square')
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+
+let result = 0
+
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove('mole')
+    })
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+
+    //assign the id of the randomPosition to the hitPosition
+    hitPosition = randomPosition.id
+}
+
+square.forEach(id => {
+    id.addEventListener('mouseup', () => {
+        if (id.id === hitPosition) {
+            result = result + 1
+            score.textContent = result
+        }
+    })
+})
